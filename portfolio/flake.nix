f537234901573908ccd19ad53a86718cc36119fa
mellivora24 <thanh139ptit@gmail.com>
2026-06-26 10:16:14 +0700

    add flake.nix

---
+++ portfolio/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for clean development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+        ];
+      };
+    };
+}
